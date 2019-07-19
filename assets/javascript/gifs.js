dc63a686ac36241b72220ee5ccfee9016c6be950
syeda zaidi <zaidisyeda03@gmail.com>
2019-07-18 17:08:55 -0700

    added ajax query

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 
-    var heroes = ["Batman", "Ironman", "WonderWoman", "Captain Marvel", "Black Panther", "Thor", "Magneto", "Flash", "Doctor Strange"];
-    console.log(heroes);
+    var superheroes = ["Batman", "Ironman", "WonderWoman", "Captain Marvel", "Black Panther", "Thor", "Magneto", "Flash", "Doctor Strange"];
+    console.log(superheroes);
 
     function createButtons() {
 
         $("#buttons").empty();
 
-        for (var i = 0; i < heroes.length; i++) {
+        for (var i = 0; i < superheroes.length; i++) {
             var btns = $("<button>");
             btns.addClass("heroBtns btn btn-info");
-            btns.attr("data-name", heroes[i]);
-            btns.text(heroes[i]);
+            btns.attr("data-name", superheroes[i]);
+            btns.text(superheroes[i]);
 
             $("#buttons").append(btns);
         }
@@ -19,9 +19,24 @@ $(document).ready(function () {
 
     createButtons();
 
-    $(document).on("click", function (){
+    function getGifs() {
 
+        var hero = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphykey + "&q=" + hero + "&limit=10&offset=0&rating=G";
+
+        console.log(hero);
         
-    })
+        $.ajax({
+
+            url: queryURL,
+            method: "GET",
+
+        }).then(function(response) {
+
+            console.log(response.data);
+        })
+    }
+
+    $(document).on("click", ".heroBtns", getGifs);
 });
 

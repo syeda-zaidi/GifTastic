151c4794c504c6d9f416f432c6ee7f1a447b4437
syeda zaidi <zaidisyeda03@gmail.com>
2019-07-19 18:15:57 -0700

    added hover function to animate gifs

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var superheroes = ["Batman","spiderman", "Ironman", "WonderWoman", "Captain Marvel", "Black Panther", "Thor", "Flash", "Doctor Strange"];
+    var superheroes = ["Batman", "spiderman", "Ironman", "WonderWoman", "Captain Marvel", "Black Panther", "Thor","Aquaman", "Doctor Strange"];
     console.log(superheroes);
 
     function createButtons() {
@@ -13,12 +13,8 @@ $(document).ready(function () {
             btns.attr("data-name", superheroes[i]);
             btns.text(superheroes[i]);
 
-            // if (btns.text("")) {
-            //     $("$buttons").append("");
-            // } else {
-
             $("#buttons").append(btns);
-            
+
         }
     };
 
@@ -27,30 +23,36 @@ $(document).ready(function () {
     function getGifs() {
 
         var hero = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphykey + "&q=" + hero + "&limit=10&offset=0&rating=G";
+        var key = "pLOyNzu4brVNGfC6NtuWfBvRqvXZAV69";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=" + hero + "&limit=10&offset=0";
 
         console.log(hero);
-        
+
         $.ajax({
 
             url: queryURL,
             method: "GET",
 
-        }).then(function(response) {
+        }).then(function (response) {
 
             var results = response.data;
             console.log(results);
 
-            for (var i=0 ; i < results.length; i++) {
+            for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div class=gifbox>");
-                
-                
+
+
                 var rating = $("<p>");
                 rating.text("Rating : " + results[i].rating);
                 gifDiv.prepend(rating);
 
                 var images = $("<img>");
-                images.attr("src", results[i].images.fixed_height.url);
+                images.attr("src", results[i].images.fixed_height_still.url);
+                images.attr("data-still", results[i].images.fixed_height_still.url);
+                images.attr("data-animate", results[i].images.fixed_height.url);
+                images.attr("data-state", "still");
+                images.addClass("gifs");
+
                 gifDiv.append(images);
 
                 $("#gif-images").prepend(gifDiv);
@@ -60,15 +62,43 @@ $(document).ready(function () {
 
     $(document).on("click", ".heroBtns", getGifs);
 
-    $("#submit-btn").on("click", function(event) {
+    $("#submit-btn").on("click", function (event) {
 
         event.preventDefault();
         var newbtn = $("#input-box").val().trim();
 
-        superheroes.push(newbtn);
+        var isempty = $("#input-box").val() === "";
+        if (!isempty) {
+            superheroes.push(newbtn);
 
-        createButtons();
+            createButtons();
+        }
+
     });
+
+    $(document).on("mouseenter", ".gifs", function () {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+
+            $(this).attr("src", ($(this).attr("data-animate")));
+            $(this).attr("data-state", "animate");
+
+        }
+    });
+
+    $(document).on("mouseleave", ".gifs", function () {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "animate") {
+
+            $(this).attr("src", ($(this).attr("data-still")));
+            $(this).attr("data-state", "still");
+        }
+    });
+
 
 });
 
